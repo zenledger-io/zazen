f0b18a2e9fb568764959a9443500117ce5c734e1
jose <jose@zenledger.io>
2022-09-21 11:23:24 -0400

    refactoring httputils

--- httputils/helpers.go
+++ httputils/helpers.go
@@ -1,7 +1,9 @@
 package httputils
 
 import (
+	"context"
 	"encoding/json"
+	"github.com/zenledger-io/zazen/log"
 	"net/http"
 )
 
@@ -9,7 +11,14 @@ var (
 	JSONContentType = "application/json; charset=utf-8"
 )
 
-func SendJSON(w http.ResponseWriter, v interface{}) error {
+func SendJSON[T any](w http.ResponseWriter, t T) error {
 	w.Header().Set("Content-Type", JSONContentType)
-	return json.NewEncoder(w).Encode(v)
+	return json.NewEncoder(w).Encode(t)
+}
+
+func SendError[T any](ctx context.Context, statusCode int, w http.ResponseWriter, t T) {
+	w.WriteHeader(statusCode)
+	if err := SendJSON(w, t); err != nil {
+		log.ContextLogger(ctx).Errorf("unable to send json: %v", err)
+	}
 }
